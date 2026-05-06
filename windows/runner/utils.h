88c6bf06111eaf9164974c0748d0609f42bebcda
Scrim <destrooyy@protonmail.com>
2026-05-06 22:33:17 +0300

    update windows/runner/utils.h

---
+++ windows/runner/utils.h
@@ -0,0 +1,11 @@
+#ifndef RUNNER_UTILS_H_
+#define RUNNER_UTILS_H_
+
+#include <string>
+#include <vector>
+
+std::string Utf8FromUtf16(const wchar_t* utf16_string);
+void CreateAndAttachConsole();
+std::vector<std::string> GetCommandLineArguments();
+
+#endif
